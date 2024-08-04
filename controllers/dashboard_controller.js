831c1da4ac9be230b72d1b2d645ec88df4efdeac
ron45700 <ron.mintz21@gmail.com>
2024-08-04 15:04:35 +0300

    dashboard controllers implementation

---
+++ controllers/dashboard_controller.js
@@ -0,0 +1,7 @@
+// admin dashboard
+export function usersIndex(req, res) {
+  res.render('../views/dashboard/users');
+}
+export function ordersIndex(req, res) {
+  res.render('../views/dashboard/orders');
+}
